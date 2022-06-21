02de66e8d64c654c73ff0e0c9d65f14c383ea68a
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-06-21 15:43:57 +0530

    Header

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,17 +1,33 @@
 import "./style.css";
 import { Link } from "react-router-dom";
-import { Logo, Search } from "../../svg";
+import {
+  ArrowDown,
+  Friends,
+  Gaming,
+  HomeActive,
+  Logo,
+  Market,
+  Menu,
+  Messenger,
+  Notifications,
+  Search,
+  Watch,
+} from "../../svg";
+import { useSelector } from "react-redux";
 export default function Header() {
+  const { user } = useSelector((user) => ({ ...user }));
+  console.log(user);
+  const color = "#65676b";
   return (
     <header>
       <div className="header_left">
-        <Link to="/" classname="header_logo">
+        <Link to="/" className="header_logo">
           <div className="circle">
             <Logo />
           </div>
         </Link>
         <div className="search search1">
-          <Search />
+          <Search color={color} />
           <input
             type="text"
             placeholder="Search Facebook"
@@ -19,8 +35,43 @@ export default function Header() {
           />
         </div>
       </div>
-      <div className="header_middle"></div>
-      <div className="headder_right"></div>
+      <div className="header_middle">
+        <Link to="/" className="middle_icon hover1 active">
+          <HomeActive />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Friends color={color} />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Watch color={color} />
+          <div className="middle_notification">9+</div>
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Market color={color} />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Gaming color={color} />
+        </Link>
+      </div>
+      <div className="header_right">
+        <Link to="/" className="profile_link hover1">
+          <img src={user.picture} alt="" />
+          <span>{user.first_name}</span>
+        </Link>
+        <div className="circle_icon hover1">
+          <Menu />
+        </div>
+        <div className="circle_icon hover1">
+          <Messenger />
+        </div>
+        <div className="circle_icon hover1">
+          <Notifications />
+          <div className="right_notification">5</div>
+        </div>
+        <div className="circle_icon hover1">
+          <ArrowDown />
+        </div>
+      </div>
     </header>
   );
 }
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import { useRef, useState } from "react";
+
 import Header from "../../components/header";
+import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
+  const [visible, setVisible] = useState(true);
+  const el = useRef(null);
+  useClickOutside(el, () => {
+    setVisible(false);
+  });
   return (
     <div>
-      {" "}
       <Header />
+
+      {visible && <div className="card" ref={el}></div>}
     </div>
   );
 }
