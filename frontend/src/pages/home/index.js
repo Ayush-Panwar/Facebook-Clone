5c0f4e1cf8b65ff92b0ff095abf07b95e120016d
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-06-26 20:13:28 +0530

    update

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,17 +1,10 @@
-import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
-import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
-  const [visible, setVisible] = useState(true);
-  const el = useRef(null);
-  useClickOutside(el, () => {
-    setVisible(false);
-  });
   const { user } = useSelector((user) => ({ ...user }));
   return (
     <div>
