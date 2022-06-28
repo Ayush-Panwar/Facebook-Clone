8de125e2913e896d8b58ad5fee74fb8b216807a9
Ayush-Panwar <ayushpanwar717@gmail.com>
2022-06-28 22:12:25 +0530

    activate account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Activate from "./pages/home/activate.js";
 import Login from "./pages/login/login";
 import Profile from "./pages/profile/profile";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
@@ -11,6 +12,7 @@ function App() {
         <Route element={<LoggedInRoutes />}>
           <Route path="/profile" element={<Profile />} exact />
           <Route path="/" element={<Home />} exact />
+          <Route path="/activate/:token" element={<Activate />} exact />
         </Route>
         <Route element={<NotLoggedInRoutes />}>
           <Route path="/login" element={<Login />} exact />
---
+++ frontend/src/pages/home/activate.js
@@ -0,0 +1,69 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./style.css";
+import Header from "../../components/header";
+import LeftHome from "../../components/home/left";
+import RightHome from "../../components/home/right";
+import Stories from "../../components/home/stories";
+import CreatePost from "../../components/createPost";
+import { useState } from "react";
+import ActivateForm from "./ActivateForm";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export default function Activate() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((user) => ({ ...user }));
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { token } = useParams();
+  useEffect(() => {
+    activateAccount();
+  }, []);
+  const activateAccount = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/activate`,
+        { token },
+        { headers: { Authorization: `Bearer ${user.token}` } }
+      );
+      setSuccess(data.message);
+      Cookies.set("user", JSON.stringify({ ...user, verified: true }));
+      dispatch({ type: "VERIFY", payload: true });
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+  return (
+    <div className="home">
+      {success && (
+        <ActivateForm
+          type="success"
+          header="Account verification succeded"
+          text={success}
+          loading={loading}
+        />
+      )}
+      {error && (
+        <ActivateForm
+          type="error"
+          header="Account verification failed"
+          text={error}
+          loading={loading}
+        />
+      )}
+      <Header />
+      <LeftHome user={user} />
+
+      <div className="home_middle">
+        <Stories />
+        <CreatePost user={user} />
+      </div>
+      <RightHome user={user} />
+    </div>
+  );
+}
